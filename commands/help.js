9e48307987a673b9a69b7200119794bf79075fe0
StolenIven <36015719+StolenIven@users.noreply.github.com>
2020-06-14 12:37:17 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
      var embed = new Discord.MessageEmbed()
             .setAuthor(`Commands:`)
-            .addFields('help', 'Brings up this embed.')
-            .addFields('ping', 'Pong!')
-            .addFields('announce', 'Announces the message in an embed.')
-            .addFields('embed', 'Puts the message you provided in an embed.')
+            .addFields("help", "Brings up this embed.")
+            .addFields("ping", "Pong!")
+            .addFields("announce", "Announces the message in an embed.")
+            .addFields("embed", "Puts the message you provided in an embed.")
             .setColor('#00FFF3')
             
         message.channel.send(embed);
