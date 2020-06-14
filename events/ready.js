355492ce57fe032d569e2ac83667923755f7c2a6
StolenIven <36015719+StolenIven@users.noreply.github.com>
2020-06-14 12:32:54 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("Big Hit Entertainment", {type: "STREAMING", url: "https://www.twitch.tv/iipromiseii"});
+    bot.user.setActivity("Big Hit Entertainment", {type: "WATCHING"});
 }
