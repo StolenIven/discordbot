4d2a9aef6a1183a02b4c55d94fecffabe99f1fea
StolenIven <36015719+StolenIven@users.noreply.github.com>
2020-06-19 23:06:51 -0400

    update

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["cat"]
+    const subReddits = ["cat", "cats"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
--- commands/dog.js
+++ commands/dog.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["dog"]
+    const subReddits = ["DOG", "rarepuppers"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
