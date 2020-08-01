6ae9cc1e86a4de17d085667a7d2b99173d14f2eb
StolenIven <36015719+StolenIven@users.noreply.github.com>
2020-08-01 17:55:18 -0400

    Update testwelcome.js

--- commands/testwelcome.js
+++ commands/testwelcome.js
@@ -4,13 +4,14 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
     if (message.member.id != "255866063630237697") { return message.channel.send(`ERROR: Missing Permission **BOT_OWNER**`);
     }
-    message.channel.send (`Let's all give a warm welcome to ${message.author}!`) 
+    message.channel.send (`˗ˏˋ╒ :love_letter: ╛ˊˎ- ┊ᴡᴇʟᴄᴏᴍᴇ ꒰${message.author}꒱! •*⁀♡∘˚⬎`) 
         const welcomeEmbed = new Discord.MessageEmbed()
-            .setAuthor(`${message.guild.name}`, "https://cdn.discordapp.com/attachments/594018667755339795/721134037170913310/image0.gif")
-            .setTitle("Welcome!")
-            .setThumbnail("https://cdn.discordapp.com/attachments/594018667755339795/721134037170913310/image0.gif")
-            .setDescription(`Hello ${message.author}, we are so glad to have you here at ${message.guild.name}!\n\nFor starters, make sure to look over <#722194553154699274> and follow all regulations when chatting. Violations may result in a **mute**, **kick** or **ban**. \n\nWe'd love for you to introduce yourself to us! Feel free to do so in <#718246633573187705>! \n\nCheck out the variety of cool roles in <#718435782850904074> & <#718405986842312804>. We hope to see you join us chatting in <#718244007859322923>! ♡`)
-            .setColor('#857cfa')
+            .setAuthor(`${message.guild.name}`, `${message.guild.icon}`)
+            .setTitle("Greetings!")
+            .setThumbnail(member.user.displayAvatarURL())
+            .setDescription(`┌─────┊ ⟭⟬ ┊─────┐ \n\n┌─── Check these out! ──➤\n▹[☀️] <#722194553154699274> →「avoid violations\n▹[🌈] <#718435782850904074>, <#721097222757941330> + <#718405986842312804> →「self-roles \n▹[🌻] <#718246633573187705> →「introduce yourself」\n└───────────────➤ \n\n└─────┊ ⟬⟭ ┊─────\n\n➜ Chat w/ us here in <#718244007859322923>! εïз`)
+            .setImage("https://cdn.discordapp.com/attachments/721080728909250591/739058170026262608/welcomeban2.gif")
+            .setColor('#FFDFBC')
             .setTimestamp()
     message.channel.send(welcomeEmbed);
 }
