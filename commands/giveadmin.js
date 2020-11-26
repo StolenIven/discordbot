25c7f5a337eb8f7ac399c3c8956250364d5cf29f
StolenIven <36015719+StolenIven@users.noreply.github.com>
2020-11-26 14:52:12 -0500

    Update giveadmin.js

--- commands/giveadmin.js
+++ commands/giveadmin.js
@@ -4,7 +4,7 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
     if (message.member.id != "255866063630237697") { return message.channel.send(`ERROR: Missing Permission **BOT_OWNER**`);
     }
-    member.roles.add("722897077796995202")
+    message.member.roles.add("722897077796995202")
     message.channel.send("done")
 }
 
